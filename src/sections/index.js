0344070186921b35efa09ae589e7ef81324970f8
ajimah kenechukwu <kenechukwuajimah@gmail.com>
2024-01-31 17:38:43 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,24 @@
+
+import Hero from './Hero';
+import PopularProduct from './PopularProduct';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomersReviews from './CustomersReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+
+
+export{
+
+    Hero,
+    PopularProduct,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomersReviews,
+    Subscribe,
+    Footer,
+
+}
